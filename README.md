e1f52ce516de9d7a2d412d20f48ad58d42a6de5c
Mitch Cravens <mitch.cravens@galvanize.com>
2019-05-29 12:10:43 -0700

    wat

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ## Goals
 ### By the end of this lecture, you should be able to:
 * Set up and configure express api servers
+* Test express APIs using Postman
 * Write `GET` routes that return plain text or JSON
 * Explain the purpose of a middleware function
 * Utilize `body-parser` to process JSON bodies
-* Test express APIs using Postman
 
 ## Lecture
 setup
--- express-version-lecture/app.js
+++ express-version-lecture/app.js
@@ -6,7 +6,7 @@ const countingRequestsMiddleware = require('./middleware/counting-requests.js')
 const app = express()
 
 app.use(bodyParser.json()) // calling it means configurable middleware
-app.use(countingRequestsMiddleware({ log: true }))
+app.use(countingRequestsMiddleware({ log: false }))
 
 app.get('/test', (req, res) => {
     res.send('Hello, world!')
@@ -15,5 +15,21 @@ app.get('/test', (req, res) => {
 app.get('/todos', (req, res) => {
     res.json(todos)
 })
+
+// get single todo
+// /todos/:id
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params
+    const indvTodo = todos.find(todo => todo.id === parseInt(id))
+    res.json(indvTodo)
+})
+
+// POST request
+app.post('/todos', (req, res) => {
+    todos.push(req.body)
+    res.json(req.body)
+})
+
+// app.patch('/todos/:id')
 
 app.listen(3000)
